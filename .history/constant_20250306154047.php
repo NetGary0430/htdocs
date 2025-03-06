1b894b5029070783475836c065635e5561947d72
Gary Netherton <garynetherton@gmail.com>
2025-03-06 15:49:57 -0700

    2nd commit

---
+++ .history/constant_20250306154047.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Constant</title>
+</head>
+<body>
+    <?php
+    # Statements to be inserted here
+    define()
+    ?>
+</body>
+</html>
---
+++ .history/constant_20250306154251.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Constant</title>
+</head>
+<body>
+    <?php
+    # Statements to be inserted here
+    define('USER', 'Mike');
+    echo '<p>Hello' . USER. '</p>'
+
+    echo '<p>You are using PHP version ' . PHP_VERSION</p>'
+    ?>
+</body>
+</html>
